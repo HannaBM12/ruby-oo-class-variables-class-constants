c0634c8dd967f8d6c1675057d4940ef14dfd32cb
HannaBM12 <Hannabmulugeta@gmail.com>
2021-01-14 09:44:08 -0500

    commit tree 6f3abc5103fdea6f74ee8ca979c8a7d891daca5c

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,22 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
+
+
+  def BRANDS
+    BRANDS.uniq!
+  end
+
+
 
   def cobble
     self.condition = "new"
